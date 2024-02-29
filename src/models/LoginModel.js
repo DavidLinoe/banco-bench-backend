35d26ceab9145a45a60fd74456e3a2d0f7b28636
David Lino <david@cucafresca.com.br>
2024-02-29 16:11:08 -0300

    select no banco de  dados retornando se tem o email e senha digitados

---
+++ src/models/LoginModel.js
@@ -0,0 +1,17 @@
+const pool = require("../db/pg");
+
+const conexao = pool;
+class LoginModel {
+  autenticar({ email, senha }) {
+    return conexao.query(`
+        SELECT
+            c.nome_cliente,
+            c.saldo_cliente  
+        FROM public.login l
+       LEFT JOIN 
+        cliente c  on l.id_cliente  = c.id_cliente 
+        WHERE email = '${email}' and senha ='${senha}'
+       `);
+  }
+}
+module.exports = LoginModel;
