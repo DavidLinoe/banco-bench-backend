adfd9e8a4257415bf8f658fa1f4ac5489fb546d0
David Lino <david@cucafresca.com.br>
2024-02-29 16:09:27 -0300

    login controller

---
+++ src/controllers/LoginController.js
@@ -0,0 +1,12 @@
+const LoginService = require("../services/loginService")
+
+const loginService = new LoginService()
+class LoginController{
+
+ 
+ 
+    login(req,res){
+    loginService.teste(req,res)
+    }   
+}
+module.exports = LoginController
