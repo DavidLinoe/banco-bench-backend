8c57f1606cf51780c2fdd9ed17feb6b78b23c257
David Lino <david@cucafresca.com.br>
2024-03-01 16:59:55 -0300

    require do user no index router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const { LoginRouter } = require("./LoginRouter")
 const { RegisterRouter } = require("./RegisterRouter")
+const { UserRouter } = require("./UserRouter")
+
+
 const indexRouter = express.Router()
 
 indexRouter.use(LoginRouter)
 indexRouter.use(RegisterRouter)
+indexRouter.use(UserRouter)
 
 module.exports = indexRouter
