9e5064b449ef97ed7097b81de7eff7bbeabef653
David Lino <david@cucafresca.com.br>
2024-03-01 17:01:51 -0300

    importando o login service no controller

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -3,8 +3,6 @@ const LoginService = require("../services/loginService")
 const loginService = new LoginService()
 class LoginController{
 
- 
- 
     login(req,res){
     loginService.teste(req,res)
     }   
