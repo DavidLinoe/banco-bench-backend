572b5b821a67362ea9da6f3c5a694cfc58252202
David Lino <david@cucafresca.com.br>
2024-03-01 17:00:18 -0300

    criando model do user

---
+++ src/models/UserModel.js
@@ -0,0 +1,18 @@
+const pool = require("../db/pg");
+
+class UserModel {
+  searchUser(id_cliente) {
+    console.log("LOGDAQUERY", id_cliente)
+    return pool.query(`
+        SELECT
+            c.nome_cliente,
+            c.saldo_cliente  
+        FROM 
+            cliente c
+        WHERE 
+            id_cliente = ${+id_cliente}
+  
+            `);
+    }
+}
+module.exports = UserModel;
