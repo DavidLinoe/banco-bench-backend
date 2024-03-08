02e914e490739be50b1c7804e7ea5e152e2aa14e
David Lino <david@cucafresca.com.br>
2024-03-08 17:02:01 -0300

    exportando o verificar Chave Controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,11 @@
 const LoginController = require("./LoginController")
 const RegisterController = require("./RegisterController")
 const UserController = require("./UserController")
+const VerificarChaveController = require("./VerificarChaveController")
 
 module.exports = { 
     LoginController,
     RegisterController,
     UserController,
+    VerificarChaveController,
 }
