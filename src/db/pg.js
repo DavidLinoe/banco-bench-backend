888b870a3a753ce8c13b82c85bf047bd675337c5
David Lino <david@cucafresca.com.br>
2024-03-08 17:01:27 -0300

    migrar para o .env

--- src/db/pg.js
+++ src/db/pg.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
  
 const pool = new Pool({
-  host: 'localhost',
+  host: 'localhost', // colocar no .env
   port: 5432,
   database: 'banco-bench',
   user: 'CucaPost',
