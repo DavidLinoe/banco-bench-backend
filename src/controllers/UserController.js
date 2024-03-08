7e2274374fc16a90cd4fdcf133228df3839083d9
David Lino <david@cucafresca.com.br>
2024-03-08 17:00:10 -0300

    identacao

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,11 +1,9 @@
 const UserService = require("../services/userService");
 
-
 const userService = new UserService();
-
 class UserController {
   user(req, res) {
-    userService.enviarModel(req, res)
+    userService.enviarModel(req, res);
   }
 }
 module.exports = UserController;
