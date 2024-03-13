62880350992188837add03379c5727364bebb367
David Lino <david@cucafresca.com.br>
2024-03-13 09:24:40 -0300

    novas rotas para as chaves pix

--- src/routes/VerificarChaveRouter.js
+++ src/routes/VerificarChaveRouter.js
@@ -4,7 +4,9 @@ const { VerificarChaveController } = require("../controllers");
 const router =  Router()
 const chave = new VerificarChaveController()
 
-router.post("/verificar/chave",chave.chave)
+router.post("/verificar/chave/telefone",chave.chaveTelefone)
+router.post("/verificar/chave/email",chave.chaveEmail)
+router.post("/verificar/chave/cpf",chave.chaveCpf)
 
 
 module.exports.VerificarChaveRouter = router;
