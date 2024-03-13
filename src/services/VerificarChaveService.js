7c1be4bc300118cd8136c0e9dab4e0a41761e6f5
David Lino <david@cucafresca.com.br>
2024-03-13 09:23:28 -0300

    novos services para autenticar as chaves

--- src/services/VerificarChaveService.js
+++ src/services/VerificarChaveService.js
@@ -3,13 +3,39 @@ const  VerificarChaveModel = require("../models/VerificarChaveModel");
 const  verificarChaveModel = new  VerificarChaveModel();
 
 class VerificarChaveService {
-  async enviarChave(req, res) {
-    const [chaveR] = (await verificarChaveModel.autenticar(req.body.dadosChave)).rows;
+  async enviarChaveTelefone(req, res) {
+    const [chaveTelefone] = (await verificarChaveModel.autenticarSenha(req.body.dadosChave)).rows;
 
-    console.log("Log no Service, req.body.. " , req.body.dadosChave);
-    console.log("Log no Service " , chaveR);
-    if (chaveR) {
-      res.status(200).json({ mensagem: "Chave Existe", ...chaveR });
+    // console.log("Log no Service, req.body.. " , req.body.dadosChave);
+    console.log("Log no Service " , chaveTelefone);
+    if (chaveTelefone) {
+      res.status(200).json({ mensagem: "Chave Telefone", ...chaveTelefone });
+    } else {
+      res.status(400).json({ mensagem: "Chave Não Existe" });
+    }
+
+  }
+
+  async enviarChaveEmail(req, res) {
+    const [chaveEmail] = (await verificarChaveModel.autenticarEmail(req.body.dadosChave)).rows;
+
+    // console.log("Log no Service, req.body.. " , req.body.dadosChave);
+    console.log("Log no Service " , chaveEmail);
+    if (chaveEmail) {
+      res.status(200).json({ mensagem: "Chave Email", ...chaveEmail });
+    } else {
+      res.status(400).json({ mensagem: "Chave Não Existe" });
+    }
+
+  }
+
+  async enviarChaveCpf(req, res) {
+    const [chaveCpf] = (await verificarChaveModel.autenticarCpf(req.body.dadosChave)).rows;
+
+    // console.log("Log no Service, req.body.. " , req.body.dadosChave);
+    console.log("Log no Service " , chaveCpf);
+    if (chaveCpf) {
+      res.status(200).json({ mensagem: "Chave Email", ...chaveCpf });
     } else {
       res.status(400).json({ mensagem: "Chave Não Existe" });
     }
