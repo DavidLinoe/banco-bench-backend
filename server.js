e4a9e93ed30c2555c857b1a994994aa1b1df95b8
David Lino <david@cucafresca.com.br>
2024-03-18 11:11:49 -0300

    teste de rota no postman

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+app.get('/rota', (req,res) => { // teste de rota no postman
+res.json({msg: "Hello World"})
+}); 
