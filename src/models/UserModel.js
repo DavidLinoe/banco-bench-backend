66315f9b7c6868ea9a815e0fa934b694b2799c2d
David Lino <david@cucafresca.com.br>
2024-03-19 11:18:19 -0300

    novo dado a ser consultado

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -2,11 +2,12 @@ const pool = require("../db/pg");
 
 class UserModel {
   searchUser(id_cliente) {
-    console.log("LOGDAQUERY", id_cliente);
+    console.log("LOGDAQUERY ", id_cliente);
     return pool.query(`
         SELECT
+            c.id_cliente,   
             c.nome_cliente,
-            c.saldo_cliente  
+            c.saldo_cliente 
         FROM 
             cliente c 
         WHERE 
