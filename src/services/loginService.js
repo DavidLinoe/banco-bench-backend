90350926c77ca2a3150061567f27a275683af6f1
David Lino <david@cucafresca.com.br>
2024-03-19 11:35:35 -0300

    JWT envia o usuario em seguranca para o front

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,4 +1,6 @@
 const LoginModel = require("../models/loginModel");
+const  jwt = require( "jsonwebtoken");
+require('dotenv').config()
 
 const loginModel = new LoginModel();
 
@@ -7,7 +9,49 @@ class LoginService {
     const [cliente] = (await loginModel.autenticar(req.body.dados)).rows;
     console.log(cliente);
     if (cliente) {
-      res.status(200).json({ mensagem: "Login Efetuado", ...cliente });
+
+      const token = jwt.sign({ 
+        
+        id:cliente.id_cliente,
+        nome:cliente.nome_cliente,
+        saldo:cliente.saldo_cliente
+
+      }, process.env.SECRET_PASS,{ expiresIn: 500 })
+    
+
+      console.log('Token: ',token)
+
+      const verifyToken = jwt.verify(token,process.env.SECRET_PASS);
+      console.log('Verify Token: ',verifyToken)
+
+
+      return res.status(200).json({ mensagem: "Usuario Enviado", token })
+
+        
+      
+
+
+
+      // return done(new TokenExpiredError('jwt expired', new Date(payload.exp * 1000)));
+
+      // if(verifyToken){
+
+      // setTimeout(() => {
+
+      //   const verifyToken2 = jwt.verify(token,SECRET);
+      //   console.log('Verify Token2: ',verifyToken2)
+
+      // },20000);
+        
+
+      // } 
+  
+
+      // res.status(200).json({ mensagem: "Login Efetuado", ...cliente });
+      // // res.status(200).json({ mensagem: "Login Efetuado Token", token });
+
+      
+
     } else {
       res.status(400).json({ mensagem: "Login Negado" });
     }
