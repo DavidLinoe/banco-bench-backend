c70c9c6bf371964f088a1377e002aa3fb61fd892
David Lino <david@cucafresca.com.br>
2024-03-20 09:27:37 -0300

    declarando o balance controller no index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const { LoginRouter } = require("./LoginRouter")
 const { RegisterRouter } = require("./RegisterRouter")
 const { UserRouter } = require("./UserRouter")
 const { VerificarChaveRouter } = require("./VerificarChaveRouter")
+const { BalanceRouter } = require("./BalanceRouter")
 
 
 const indexRouter = express.Router()
@@ -11,5 +12,6 @@ indexRouter.use(LoginRouter)
 indexRouter.use(RegisterRouter)
 indexRouter.use(UserRouter)
 indexRouter.use(VerificarChaveRouter)
+indexRouter.use(BalanceRouter)
 
 module.exports = indexRouter
