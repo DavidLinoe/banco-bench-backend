8d0dde8638418d08866276a8f7cfe027a29a231c
David Lino <david@cucafresca.com.br>
2024-03-20 09:25:26 -0300

    exportando balance controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,11 +1,13 @@
-const LoginController = require("./LoginController")
-const RegisterController = require("./RegisterController")
-const UserController = require("./UserController")
-const VerificarChaveController = require("./VerificarChaveController")
+const LoginController = require("./LoginController");
+const RegisterController = require("./RegisterController");
+const UserController = require("./UserController");
+const VerificarChaveController = require("./VerificarChaveController");
+const BalanceController = require("./BalanceController");
 
-module.exports = { 
-    LoginController,
-    RegisterController,
-    UserController,
-    VerificarChaveController,
-}
+module.exports = {
+  LoginController,
+  RegisterController,
+  UserController,
+  VerificarChaveController,
+  BalanceController,
+};
