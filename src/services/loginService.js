26444c2f17b056cc5a65563257ce91ecbdea082f
David Lino <david@cucafresca.com.br>
2024-03-25 15:19:35 -0300

    logica bcripty verifica se senha é igual ao hash

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,31 +1,61 @@
 const LoginModel = require("../models/loginModel");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const bcrypt = require("bcrypt");
 
 const loginModel = new LoginModel();
 
 class LoginService {
   async teste(req, res) {
-    const [cliente] = (await loginModel.autenticar(req.body.dados)).rows;
-    console.log(cliente);
-    if (cliente) {
-      const token = jwt.sign(
-        {
-          id: cliente.id_cliente,
-        },
-        process.env.SECRET_PASS,
-        { expiresIn: 2000 }
-      );
+    // const [cliente] = (await loginModel.autenticar(req.body.dados)).rows;
 
-      console.log("Token: ", token);
+    // const compareS = await bcrypt.compare(req.body.dados.senha, senhaHash);
+    // console.log("senha Verificada ",compareS);
 
-      const verifyToken = jwt.verify(token, process.env.SECRET_PASS);
-      console.log("Verify Token: ", verifyToken);
+    try {
+      const [email] = (await loginModel.autenticarEmail(req.body.dados.email))
+        .rows;
 
-      return res.status(200).json({ mensagem: "Usuario Enviado", token });
-    } else {
+      console.log(email.senha, " senha verificado");
+
+      if (email) {
+        const compareS = await bcrypt.compare(
+          req.body.dados.senha,
+          email.senha
+        );
+        console.log("log na senha digitada", req.body.dados.senha);
+
+        console.log("log na senha retornada do email", compareS);
+
+        if (compareS) {
+          console.log("Senha Hash Confirmada");
+        }
+
+        if (email && compareS) {
+          const token = jwt.sign(
+            {
+              id: email.id_cliente,
+            },
+            process.env.SECRET_PASS,
+            { expiresIn: 2000 }
+          );
+
+          console.log("Token: ", token);
+
+          const verifyToken = jwt.verify(token, process.env.SECRET_PASS);
+          console.log("Verify Token: ", verifyToken);
+
+          return res.status(200).json({ mensagem: "Usuario Enviado", token });
+        } else {
+          res.status(400).json({ mensagem: "Login Negado" });
+        }
+      }
+    } catch (error) {
+      console.error(error);
       res.status(400).json({ mensagem: "Login Negado" });
     }
+
+    // console.log(cliente);
   }
 }
 module.exports = LoginService;
