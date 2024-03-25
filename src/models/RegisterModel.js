ea21793831f73bbcd2aaae72d587c786b645ed70
David Lino <david@cucafresca.com.br>
2024-03-25 15:18:09 -0300

    select dos dados cpf, email, telefone, isso verifica um por um se ja existe, para evitar cadastrar duplicidade

--- src/models/RegisterModel.js
+++ src/models/RegisterModel.js
@@ -2,6 +2,48 @@ const pool = require("../db/pg");
 
 const conexao = pool;
 class RegisterModel {
+
+  consultarClienteEtapa1( email) {
+    return conexao.query(`
+    
+    SELECT
+        l.email
+    FROM 
+        login l 
+    WHERE  
+        l.email = '${email}' 
+        
+        `);
+  }
+
+  consultarClienteEtapa2( cpf ) {
+    return conexao.query(`
+    
+    SELECT
+        l.cpf
+    FROM 
+        login l 
+    WHERE  
+        l.cpf = '${cpf}' 
+        
+        `);
+  }
+
+
+  consultarClienteEtapa3( telefone ) {
+    return conexao.query(`
+    
+    SELECT
+        l.telefone
+    FROM 
+        login l 
+    WHERE  
+        l.telefone = '${telefone}' 
+        
+        `);
+  }
+
+
   insertCliente({ nome }) {
     return conexao.query(`
     INSERT INTO public.cliente
@@ -10,7 +52,7 @@ class RegisterModel {
     
        `);
   }
-  insertLogin({ telefone, email, senha, id_cliente, cpf}) {
+  insertLogin({ telefone, email, senha, id_cliente, cpf }) {
     return conexao.query(`
     
     INSERT INTO public.login
@@ -18,6 +60,21 @@ class RegisterModel {
     VALUES('${email}', '${senha}', '${telefone}', ${id_cliente}, '${cpf}');
     
        `);
+  }
+  insertLoginSenha(id, senha) {
+    console.log("Log query Senha", senha);
+    console.log("Log query ID", id);
+
+    return conexao.query(`
+    
+    UPDATE
+    public.login
+    SET
+      senha = '${senha}'
+    WHERE
+      id_cliente = '${id}'
+      
+      `);
   }
 }
 
