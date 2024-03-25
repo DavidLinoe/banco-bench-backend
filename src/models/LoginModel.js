c58f53508bc6cab1ef57ef531103b4dda43b98d0
David Lino <david@cucafresca.com.br>
2024-03-25 15:17:23 -0300

    verifica somente o email no banco e com esse email pega o hash da senha e verifica se esta true

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -2,17 +2,20 @@ const pool = require("../db/pg");
 
 const conexao = pool;
 class LoginModel {
-  autenticar({ email, senha }) {
+  autenticarEmail( email ) {
     return conexao.query(`
         SELECT
             c.nome_cliente,
             c.saldo_cliente, 
-            c.id_cliente
+            c.id_cliente,
+            l.senha
         FROM public.login l
        LEFT JOIN 
         cliente c  on l.id_cliente  = c.id_cliente 
-        WHERE email = '${email}' and senha ='${senha}'
+        WHERE email = '${email}'
        `);
   }
+
+
 }
 module.exports = LoginModel;
